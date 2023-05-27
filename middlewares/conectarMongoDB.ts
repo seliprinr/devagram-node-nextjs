33699e983403346da60761cca7f6bfc7ca9ce7c2
Rodrigo Seliprin <rseliprin@gmail.com>
2023-05-27 07:49:35 -0300

    configuracao do banco de dados

---
+++ middlewares/conectarMongoDB.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import mongoose, { mongo } from 'mongoose';
+import type { RespostaPadraoMsg } from '../types/RespostaPadraoMsg';
+
+export const conectarMongoDB = (handler : NextApiHandler) =>
+    async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
+
+        // Verificar se o banco já está conectado, se estiver seguir para o endpoint ou próximo middleware
+        if(mongoose.connections[0].readyState){
+            return handler(req, res);
+        }
+
+        // Já que não está conectado, vamos conectar
+        // Obter a variável de ambiente preenchida do env
+        const {DB_CONEXAO_STRING} = process.env;
+        
+        // Se a env estiver vazia aborta o uso do sistema e avisa o programador
+        if(!DB_CONEXAO_STRING){
+            return res.status(500).json({ erro : 'ENV de configuração do banco, não informado'});
+        }
+
+
+        mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+        mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+        await mongoose.connect(DB_CONEXAO_STRING);
+
+        // Agora posso seguir para o endpoint pois estou conectado no banco
+        return handler(req, res);
+    }
